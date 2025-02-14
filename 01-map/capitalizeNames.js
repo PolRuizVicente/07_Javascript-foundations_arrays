ed3304b4eca6387ad11d6aaf449c8ec5bbbd9103
PolRuizVicente <145784512+PolRuizVicente@users.noreply.github.com>
2025-02-14 16:30:28 +0100

    Update capitalizeNames.js

--- 01-map/capitalizeNames.js
+++ 01-map/capitalizeNames.js
@@ -11,3 +11,7 @@
  * @param {string[]} namesList - An array of names to be capitalized.
  * @returns {string[]} An array of names with the first letter capitalized and the rest in lowercase.
  */
+
+export function capitalizeMates(namesList) {
+    return namesList.map((name) => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
+}
