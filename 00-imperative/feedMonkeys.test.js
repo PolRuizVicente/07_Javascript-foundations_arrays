31ae18a52b1aedfd85a6abe4f605f1c5084417a7
PolRuizVicente <145784512+PolRuizVicente@users.noreply.github.com>
2025-02-14 16:28:40 +0100

    Update feedMonkeys.test.js

--- 00-imperative/feedMonkeys.test.js
+++ 00-imperative/feedMonkeys.test.js
@@ -1,17 +1,113 @@
+import { describe, it, expect } from "vitest";
+import { feedMonkeys } from "./feedMonkeys";
+
 const MONKEYS_LIST = ["🐒", "🦍", "🦧"];
-const apple = "🍎";
-const banana = "🍌";
-const grape = "🍇";
 const specialCharacters = "🍉🍇";
 
 describe("feedMonkeys", () => {
-  it.todo("should feed monkeys with bananas");
+  it("should feed monkeys with bananas", () => {
 
-  it.todo("should feed monkeys with apples");
+      // Arrange
 
-  it.todo("should feed monkeys with grapes");
+      const banana = "🍌";
 
-  it.todo("should return an empty array if no fruit is provided");
+      const expected = [
+          "🐒 eats 🍌",
+          "🦍 eats 🍌",
+          "🦧 eats 🍌"
+      ];
 
-  it.todo("should handle feeding monkeys with special characters");
+      // Act
+
+      const result = feedMonkeys(MONKEYS_LIST, banana);
+
+      // Assert
+
+      expect(result).toEqual(expected);
+
+  });
+
+  it("should feed monkeys with apples", () => {
+
+    // Arrange
+
+    const apple = "🍎";
+
+    const expected = [
+        "🐒 eats 🍎",
+        "🦍 eats 🍎",
+        "🦧 eats 🍎"
+    ];
+
+    // Act
+
+    const result = feedMonkeys(MONKEYS_LIST, apple);
+
+    // Assert
+
+    expect(result).toEqual(expected);
+
+});
+
+   it("should feed monkeys with grapes", () => {
+
+    // Arrange
+
+    const grape = "🍇";
+
+    const expected = [
+        "🐒 eats 🍇",
+        "🦍 eats 🍇",
+        "🦧 eats 🍇"
+    ];
+
+    // Act
+
+    const result = feedMonkeys(MONKEYS_LIST, grape);
+
+    // Assert
+
+    expect(result).toEqual(expected);
+
+});
+
+  it("should return an empty array if no fruit is provided", () => {
+
+  
+    // Arrange
+
+    const expected = [
+        "🐒 eats ",
+        "🦍 eats ",
+        "🦧 eats "
+    ];
+
+    // Act
+
+    const result = feedMonkeys(MONKEYS_LIST, "");
+
+    // Assert
+
+    expect(result).toEqual(expected);
+
+});
+
+  it("should handle feeding monkeys with special characters", () => {
+
+    // Arrange
+
+    const expected = [
+        "🐒 eats 🍉🍇",
+        "🦍 eats 🍉🍇",
+        "🦧 eats 🍉🍇"
+    ];
+
+    // Act
+
+    const result = feedMonkeys(MONKEYS_LIST, specialCharacters);
+
+    // Assert
+    expect(result).toEqual(expected);
+
+    });
 });
