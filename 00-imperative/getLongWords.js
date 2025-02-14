96a873bb42c944f77db265a0953352b08794a78f
PolRuizVicente <145784512+PolRuizVicente@users.noreply.github.com>
2025-02-14 16:29:07 +0100

    Update getLongWords.js

--- 00-imperative/getLongWords.js
+++ 00-imperative/getLongWords.js
@@ -2,9 +2,10 @@
 // const BEATLES = ["John", "George", "Paul", "Ringo"];
 
 /**
- * Given 'WORDS' array, define function 'getLongWords' to return an array only with strings with length equal or greater then 5
+ * Given 'WORDS' array, define function 'getLongWords' to return an array only with strings with length equal or greater than 5
  * use for() instruction
  */
+
 
 /**
  * Returns an array of strings with length equal or greater than 5.
@@ -12,3 +13,17 @@
  * @param {string[]} wordsList - The array of words to filter.
  * @returns {string[]} An array containing words with length equal or greater than 5.
  */
+
+
+export default function getLongWords(wordsList){
+    const longWords = [];
+
+    for (let i = 0; i < wordsList.length; i++) {
+        if (wordsList[i].length >= 5) {
+            longWords.push(wordsList[i]);
+        }
+    }
+    
+    return longWords;
+};
+
