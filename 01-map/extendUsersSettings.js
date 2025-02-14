16671f4a2368a3959343ba25c5f4dc379323b23c
PolRuizVicente <145784512+PolRuizVicente@users.noreply.github.com>
2025-02-14 16:31:18 +0100

    Update extendUsersSettings.js

--- 01-map/extendUsersSettings.js
+++ 01-map/extendUsersSettings.js
@@ -59,3 +59,12 @@ const USERS = [
  * @param {Array<Object>} users - The array of user objects to extend.
  * @returns {Array<Object>} An array of user objects with extended settings.
  */
+
+export function extendUsersSettings(users) {
+    return users.map((user, index) => ({
+        ...user,
+        id: index,
+        isEnabled: true,
+    }));
+    }
+
